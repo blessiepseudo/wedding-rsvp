309435f837bf0159e6ea048e2f093abb5b9a34c9
blessiepseudo <blessiepseudo@gmail.com>
2026-06-26 13:11:19 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,171 @@
+const API =
+"PASTE_YOUR_GOOGLE_SCRIPT_URL_HERE";
 
+let selectedSeats=0;
+
+
+function searchGuest(){
+
+let query =
+document.getElementById("name").value;
+
+
+if(query.length < 2){
+return;
+}
+
+
+fetch(API,{
+
+method:"POST",
+
+body:JSON.stringify({
+
+action:"search",
+query:query
+
+})
+
+})
+
+
+.then(r=>r.json())
+
+.then(data=>{
+
+
+let box =
+document.getElementById("suggestions");
+
+
+box.innerHTML="";
+
+
+data.forEach(g=>{
+
+
+let div =
+document.createElement("div");
+
+
+div.className="suggestion";
+
+
+div.innerHTML=g.name;
+
+
+div.onclick=function(){
+
+selectGuest(g);
+
+};
+
+
+box.appendChild(div);
+
+
+});
+
+
+});
+
+
+}
+
+
+
+function selectGuest(g){
+
+
+document.getElementById("name").value=g.name;
+
+
+selectedSeats=g.seats;
+
+
+document.getElementById("guestInfo")
+.innerHTML =
+"Reserved Seats: "+g.seats;
+
+
+document.getElementById("suggestions")
+.innerHTML="";
+
+
+}
+
+
+
+function submitRSVP(){
+
+
+let name =
+document.getElementById("name").value;
+
+
+let rsvp =
+document.getElementById("rsvp").value;
+
+
+let requests =
+document.getElementById("requests").value;
+
+
+
+if(!name || !rsvp){
+
+alert("Complete the form");
+
+return;
+
+}
+
+
+
+fetch(API,{
+
+method:"POST",
+
+body:JSON.stringify({
+
+action:"submit",
+
+name:name,
+
+rsvp:rsvp,
+
+seats:selectedSeats,
+
+requests:requests
+
+})
+
+})
+
+
+.then(r=>r.json())
+
+.then(result=>{
+
+
+if(result.status==="duplicate"){
+
+document.getElementById("message")
+.innerHTML =
+"Already submitted.";
+
+}
+
+else{
+
+document.getElementById("message")
+.innerHTML =
+"Thank you! RSVP saved.";
+
+}
+
+
+});
+
+
+}
